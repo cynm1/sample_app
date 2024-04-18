9d2d758ed59124ad7f7ec23697035feedf053053
cynm1 <163312272+cynm1@users.noreply.github.com>
2024-04-18 13:16:50 +0000

    [Finish] 3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,10 +10,15 @@ class ListsController < ApplicationController
    list.save
    redirect_to '/top'
   end
+  
+  def index
+    @lists = List.all
+  end
 
   private
 
   def list_params
     params.require(:list).permit(:title, :body)
   end
+  
 end
